ad80fd953f857f0bfa59ddcf37647a5b641dbd75
Josiah Hester <josiah.hester@gmail.com>
2017-11-21 15:23:43 -0600

    One more cleanup change.

--- firmware/exp6989/main.c
+++ firmware/exp6989/main.c
@@ -10,9 +10,7 @@ int main (void)
 	uint8_t i=0;
 	siren_command("PRINTF: Start\r\n");    
 	while(1) {
-	siren_command("PRINTF: ADC Read      %u\r\n", i++);    
-    long* __w_ptr_argh = (long*)0x10000;
-    *__w_ptr_argh = 0xDEED;	
+	siren_command("PRINTF: Loop index%u\r\n", i++);    
 	__delay_cycles(1000);
 	}
 }

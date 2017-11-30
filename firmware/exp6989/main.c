5612dc658b99907aec68eb055e7dcbeef6e22310
Josiah Hester <josiah.hester@gmail.com>
2017-11-30 09:57:40 -0600

    Firmware to test out MPU

--- firmware/exp6989/main.c
+++ firmware/exp6989/main.c
@@ -8,9 +8,23 @@ int main (void)
 {
 	WDTCTL = WDTPW | WDTHOLD;                 // Stop WDT
 	uint8_t i=0;
-	siren_command("PRINTF: Start\r\n");    
+	siren_command("PRINTF: Start\r\n");
+	MPUCTL0  = MPUPW;
+    MPUSEGB1 = 0x4400 >> 4;
+    MPUSEGB2 = 0x023FFF >> 4;
+    MPUSAM   &= ~MPUSEG2WE;
+    MPUCTL0  = MPUPW | MPUENA | MPUSEGIE;
+
+    unsigned long *ptr = (unsigned long *)0x4401;
+
 	while(1) {
-	siren_command("PRINTF: Loop index%u\r\n", i++);    
-	__delay_cycles(1000);
+		siren_command("PRINTF: Loop index%u\r\n", i++);    
+		*ptr =1;
+
+		// Check access violation
+		if(MPUCTL1 & MPUSEG2IFG) {
+			siren_command("PRINTF: Bad write to segment 2, addr: %x\r\n", ptr);    
+		}
+		__delay_cycles(1000);
 	}
 }
